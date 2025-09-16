271e1a007b9137ce160c79f0a989444ade4e5503
bangdiep <dtkhanhbang.04204@gmail.com>
2025-09-16 14:09:12 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using SmartBuildingConsoleApp.DigitalTwins;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using Azure;
+using Azure.DigitalTwins.Core;
+
+
+namespace SmartBuildingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalTwinsManager dtHelper = new DigitalTwinsManager();
+
+            // TODO
+            string[] paths = new string[] {
+                "Sensors/sensor.json"
+            };
+
+            foreach (var path in paths)
+            {
+                if (File.Exists(path))
+                {
+                    dtHelper.CreateModel(path);
+                }
+                else
+                {
+                    Console.WriteLine($"⚠️ File not found: {path}");
+                }
+            }
+
+
+
+        }
+    }
+}
